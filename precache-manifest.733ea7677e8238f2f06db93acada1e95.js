a47caade4d2b45de325e7de4b72924c61409d49b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-05 13:18:23 +0000

    Deploy albertviilik/albertviilik.github.io to github.com/albertviilik/albertviilik.github.io.git:master

---
+++ precache-manifest.733ea7677e8238f2f06db93acada1e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92f3885b66f8b4c213891549c48ff43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "067cb9ddac4f000aaae4",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "e25c004f6f5501563766",
+    "url": "/static/js/2.a010f758.chunk.js"
+  },
+  {
+    "revision": "067cb9ddac4f000aaae4",
+    "url": "/static/js/main.fb00ac2a.chunk.js"
+  },
+  {
+    "revision": "08736ae4972a9ea1cd83",
+    "url": "/static/js/runtime-main.80a3cd77.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
