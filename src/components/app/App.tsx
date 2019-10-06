0dde2ca038251a1979120ea8c0049ab4c187def8
Albert Viilik <albert.viilik@gmail.com>
2019-10-06 16:33:35 +0100

    Made app use new Dock and removed unused import statements

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Desktop} from "../desktop/desktop";
 import {Dock} from "../dock/dock";
 
 const App: React.FC = () => {
     return (
         <div className="App">
+            <Dock buttons={"Folder"}/>
         </div>
     );
 };
