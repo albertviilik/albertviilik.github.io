50024d07c02a40d688dcf8d8e8836a003cea5ff5
Albert Viilik <albert.viilik@gmail.com>
2019-10-06 15:57:25 +0100

    Updated the App to just be a simple grey background page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,9 +6,6 @@ import {Dock} from "../dock/dock";
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Desktop>
-                <Dock/>
-            </Desktop>
         </div>
     );
 };
