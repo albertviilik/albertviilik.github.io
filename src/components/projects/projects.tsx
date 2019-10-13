128498472670d9529b262a2ec2cc665f13ccd8d0
Albert Viilik <albert.viilik@gmail.com>
2019-10-13 20:34:37 +0100

    Added Projects Page

---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,14 @@
+import './projects.css'
+
+import React from 'react';
+import {Paper} from "@material-ui/core";
+
+const Projects = () => {
+    return (
+        <Paper className={'projects_area span_2_of_4'}>
+            <p>Projects Page</p>
+        </Paper>
+    );
+};
+
+export default Projects;
