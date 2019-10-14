cf1fe1bd29bd28399ab57b741308f02ec0fd6147
Albert Viilik <albert.viilik@gmail.com>
2019-10-14 17:22:37 +0100

    Turned Personal Info Page Into a Card

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -1,7 +1,6 @@
 import './info.css'
 
 import React from 'react';
-import {Paper} from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
@@ -12,40 +11,49 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 
 const Info = () => {
     return (
-        <Paper className={'info_area span_1_of_4'}>
-            <List>
-                <ListItem>
-                    <ListItemIcon>
-                        <PersonOutlineIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary={<Typography>Albert Viilik</Typography>} secondary={null}/>
-                </ListItem>
-                <Divider/>
-                <ListItem>
-                    <ListItemIcon>
-                        <PhoneIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary={<Typography>+44 (0) 7585 830006</Typography>} secondary={null}/>
-                </ListItem>
-                <Divider/>
-                <ListItem>
-                    <ListItemIcon>
-                        <EmailIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary={<Typography>albert.viilik@gmail.com</Typography>} secondary={null}/>
-                </ListItem>
-                <Divider/>
-                <ListItem>
-                    <ListItemIcon>
-                        <LocationOnIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary={<Typography>Manchester, UK</Typography>} secondary={null}/>
-                </ListItem>
-            </List>
-        </Paper>
+        <Card className={'info_area span_1_of_4'}>
+            <CardActionArea>
+                <CardMedia className={'card_media'} image={'./albert.jpg'} title={'Albert Viilik'}/>
+                <CardContent>
+                    <List>
+                        <ListItem>
+                            <ListItemIcon>
+                                <PersonOutlineIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={<Typography>Albert Viilik</Typography>} secondary={null}/>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem>
+                            <ListItemIcon>
+                                <PhoneIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={<Typography>+44 (0) 7585 830006</Typography>} secondary={null}/>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem>
+                            <ListItemIcon>
+                                <EmailIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={<Typography>albert.viilik@gmail.com</Typography>} secondary={null}/>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem>
+                            <ListItemIcon>
+                                <LocationOnIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary={<Typography>Manchester, UK</Typography>} secondary={null}/>
+                        </ListItem>
+                    </List>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 };
 
